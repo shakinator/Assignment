03ab9abff64dc3a7ded0cc518c9d55f6aef12707
shakinator <65782416+shakinator@users.noreply.github.com>
2021-05-26 16:52:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import APPs from './APPs'
 import reportWebVitals from './reportWebVitals';
 
